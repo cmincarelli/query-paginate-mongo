105c53e4e655453f9e763613de59b39005f53ecd
Chris Mincarelli <chrism@theancholight.com>
2017-01-08 14:17:26 -0500

    bug fix - config

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 'use strict';
+
 var MongoQS = require('mongo-querystring');
 var queryToMongo = require('query-to-mongo');
 var merge = require('lodash.merge');
-module.exports = function QueryPageMongo(options) {
-  const opts = options || {};
+
+var QueryPageMongo = function (options) {
+  let opts = options || {};
+  opts = merge(opts,{ignore: ['near', 'bbox', 'after', 'before', 'between']});
   let mongoQS = new MongoQS(opts);
+
   return function parse(config){
-    let q2 = mongoQS.parse(req.query);
-    let q1 = queryToMongo(req.query,opts);
+    let q2 = mongoQS.parse(config);
+    let q1 = queryToMongo(config, opts);
     ['fields','sort','limit','offset'].forEach(function(field){
       delete q2[field];
     });
     q1.criteria = merge(q1.criteria, q2);
     return q1;
   }
+
 };
+
+module.exports = QueryPageMongo;
