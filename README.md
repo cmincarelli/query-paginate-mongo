e03f0ae46c7ff920d2cacd0e58c0fba7dc9ce06b
Chris Mincarelli <chrism@theancholight.com>
2017-01-09 14:14:33 -0500

    enhance fuctionality and embed common functionality

--- README.md
+++ README.md
@@ -6,52 +6,44 @@ Combines two other great projects ("mongo-querystring" and "query-to-mongo") and
 ### Sample
 
 ```javascript
-// ?near=-115.01,36.065,90000&sort=name&limit=10&offset=1
+// group.controller.js
 
 import QPM from 'query-paginate-mongo';
 import Group from './group.model';
 
 let qpm = new QPM({
+  id: '_id',                        // custom id field
   custom: {
-    bbox: 'location.coords.point',// your geometry field
-    near: 'location.coords.point',// your geometry field
-    after: 'updated_at'           // your last modified field
+    bbox:  'location.coords.point', // your geometry field
+    near:  'location.coords.point', // your geometry field
+    after:  'updated_at'            // your last modified field
+    before: 'updated_at'            // your last modified field
   }
 });
 
 export function index(req, res) {
-
-  let query = qpm(req.query);
-
-  let collection = Group.find(query.criteria);
-
-  Group.count(query.criteria, function(err, count){
-
-    if(query.options.fields){
-      collection.select(query.options.fields);
-    }
-    if(query.options.sort){
-      collection.sort(query.options.sort);
-    }
-    if(query.options.limit){
-      collection.limit(query.options.limit);
-    }
-    if(query.options.offset){
-      collection.skip(query.options.offset);
-    }
-
-    return collection.exec()
-    .then(function(entity){
-      let pagination = query.links( process.env.DOMAIN + '/api/v1/groups', count);
-      let response = {
-        'groups': entity,
-      };
-      if(null !== pagination){ response.pagination = pagination; }
-      return res.status(200).json(response);
-    })
-    .catch(handleError(res));
+  qpm
+  .parse(req.query, Group, function(err, query){
+    query
+      .setEnvelope('groups', '/api/groups')
+      .exec(function(err, groups){
+        if(err) return res.json(500, error);
+        return res.json(groups);
+      });
   });
 
+}
+
+export function show(req, res) {
+  req.query.id = req.params.oid;
+  qpm
+  .parse(req.query, Group, function(err, query){
+    query
+      .exec(function(error, group){
+        if(error) return res.json(500, error);
+        return res.json(group);
+      });
+  });
 }
 ```
 
--- index.js
+++ index.js
@@ -1,30 +1,143 @@
 'use strict';
-
-var MongoQS = require('mongo-querystring');
-var queryToMongo = require('query-to-mongo');
-var merge = require('lodash.merge');
-
-var QueryPageMongo = function (options) {
-  let opts = options || {};
-  opts = merge(opts,{ignore: ['near', 'bbox', 'after', 'before', 'between']});
-  let mongoQS = new MongoQS(opts);
-
-  return function parse(config){
-    let query = {options:{}};
-    if(config.populate){
-      query.options.populate = config.populate.split(',').join(' ');
-      delete config.populate;
+let MongoQS = require('mongo-querystring');
+let queryToMongo = require('query-to-mongo');
+let _ = require('lodash');
+let q = require('Q');
+let QueryPageMongo = function(options) {
+    let opts = options || {};
+    opts = _.extend({ id: '_id', ignore: ['near', 'bbox', 'after', 'before', 'between'] }, opts);
+    let mongoQS = new MongoQS(opts);
+    return {
+        parse: function(params, collection, callback) {
+            let query = { options: {} };
+            if (params.envelope) {
+                query.options.envelope = params.envelope;
+                delete params.envelope;
+            }
+            if (params.populate) {
+                query.options.populate = params.populate.split(',').join(' ');
+                delete params.populate;
+            }
+            let mqs = mongoQS.parse(params);
+            let qtm = queryToMongo(params, opts);
+            ['fields', 'sort', 'limit', 'offset'].forEach(function(field) {
+                delete mqs[field];
+            });
+            qtm.criteria = _.extend(qtm.criteria, mqs);
+            query = _.extend(query, qtm);
+            if (query.criteria.id) {
+                var deferred = q.defer();
+                var response = {
+                    exec: function(callback) {
+                        let ques = {};
+                        ques[opts.id] = query.criteria.id;
+                        let docs = collection.findOne(ques);
+                        if (query.options.fields) {
+                            docs.select(query.options.fields);
+                        }
+                        let that = this;
+                        return docs
+                            .exec()
+                            .then(function(_docs) {
+                                if (callback) {
+                                    callback(null, _docs);
+                                }
+                                console.log(_docs);
+                                return _docs;
+                            })
+                            .catch(function(error) {
+                                if (callback) {
+                                    callback(error);
+                                }
+                                console.log(error);
+                                return error;
+                            });
+                    }
+                };
+                deferred.resolve(response);
+                if (callback) {
+                    callback(null, response);
+                }
+                return deferred.promise;
+            }
+            return collection
+                .count(query.criteria)
+                .then(function(count) {
+                    let response = {
+                        setEnvelope: function(envelope, linkPath) {
+                            if (envelope && 'string' === typeof envelope) {
+                                query.options.envelope = envelope;
+                            }
+                            if (linkPath && 'string' === typeof linkPath) {
+                                query.options.linkPath = linkPath;
+                            }
+                            return this;
+                        },
+                        getCount: function() {
+                            return count;
+                        },
+                        getLinks: function(path) {
+                            return query.links(path, count);
+                        },
+                        exec: function(callback) {
+                            let docs = collection.find(query.criteria);
+                            if (query.options.fields) {
+                                docs.select(query.options.fields);
+                            }
+                            if (query.options.sort) {
+                                docs.sort(query.options.sort);
+                            }
+                            if (query.options.limit) {
+                                docs.limit(query.options.limit);
+                            }
+                            if (query.options.offset) {
+                                docs.skip(query.options.offset);
+                            }
+                            let that = this;
+                            let response = docs
+                                .exec()
+                                .then(function(_docs) {
+                                    if (query.options.envelope) {
+                                        let enveloped = {};
+                                        enveloped[query.options.envelope] = _docs;
+                                        if (query.options.linkPath) {
+                                            let links = that.getLinks(query.options.linkPath);
+                                            if (links) {
+                                                enveloped.links = links;
+                                            }
+                                        }
+                                        if (callback) {
+                                            callback(null, enveloped);
+                                        }
+                                        return enveloped;
+                                    } else {
+                                        if (callback) {
+                                            callback(null, _docs);
+                                        }
+                                        return _docs;
+                                    }
+                                })
+                                .catch(function(error) {
+                                    if (callback) {
+                                        callback(error);
+                                    }
+                                    return error;
+                                });
+                            return response;
+                        }
+                    }
+                    if (callback) {
+                        callback(null, response)
+                    }
+                    return response;
+                })
+                .catch(function(error) {
+                    if (callback) {
+                        callback(error);
+                    }
+                    return error;
+                });
+        }
     }
-    let q2 = mongoQS.parse(config);
-    let q1 = queryToMongo(config, opts);
-    ['fields','sort','limit','offset'].forEach(function(field){
-      delete q2[field];
-    });
-    q1.criteria = merge(q1.criteria, q2);
-    query = merge(query,q1);
-    return query;
-  }
-
 };
-
 module.exports = QueryPageMongo;
